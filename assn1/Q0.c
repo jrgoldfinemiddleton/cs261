513fada45dfa4a9630b60419637a5c348b8a7c0f
Jason Goldfine-Middleton <jrgoldfinemiddleton@gmail.com>
2016-01-07 13:24:04 -0800

    Removed print labels in Q0.c

--- assn1/Q0.c
+++ assn1/Q0.c
@@ -9,13 +9,13 @@
 
 void fooA(int* iptr){
     /*Print the value of the integer pointed to by iptr*/
-    printf("Value of integer pointed to by iptr: %d\n", *iptr);
+    printf("%d\n", *iptr);
      
     /*Print the address pointed to by iptr*/
-    printf("Address of integer pointed to by iptr: %p\n", iptr);
+    printf("%p\n", iptr);
      
     /*Print the address of iptr itself*/
-    printf("Address of iptr: %p\n", &iptr);
+    printf("%p\n", &iptr);
 }
 
 int main(){
@@ -24,13 +24,13 @@ int main(){
     int x = 11;
     
     /*print the address of x*/
-    printf("Address of x: %p\n", &x);
+    printf("%p\n", &x);
     
     /*Call fooA() with the address of x*/
     fooA(&x);
     
     /*print the value of x*/
-    printf("Value of x: %d\n", x);
+    printf("%d\n", x);
     
     return 0;
 }
